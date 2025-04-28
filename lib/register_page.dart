08d7f45d845e191b02addb1ff68c4554968633de
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-28 21:02:36 +0700

    Membuat body form input register dengan validasi

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -12,7 +12,8 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController nohpController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final TextEditingController confirmpasswordController = TextEditingController();
+  final TextEditingController confirmpasswordController =
+      TextEditingController();
   bool obscurepw = true;
 
   @override
@@ -20,15 +21,252 @@ class _RegisterPageState extends State<RegisterPage> {
     super.initState();
   }
 
-  void liatpw(){
+  void liatpw() {
     setState(() {
-       obscurepw = !obscurepw;
+      obscurepw = !obscurepw;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
     return Scaffold(
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 80.0),
+                  child: Center(
+                    child: Image(
+                      image: AssetImage('assets/images/pokebal.png'),
+                      height: 100,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: Text(
+                    'DAFTAR AKUN BARU',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ),
+                SizedBox(height: 30),
+
+                Text('Nama Lengkap'),
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: namaController,
+                  decoration: InputDecoration(
+                    hintText: 'Nama Lengkap',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 10, 86, 216),
+                        width: 3.0,
+                      ),
+                    ),
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Nama Lengkap tidak boleh kosong';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Email'),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              hintText: 'Email',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(15),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(15),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 10, 86, 216),
+                                  width: 3.0,
+                                ),
+                              ),
+                              prefixIcon: Icon(Icons.email),
+                            ),
+                            autovalidateMode: AutovalidateMode.onUserInteraction,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Email tidak boleh kosong';
+                              }
+                              return null;
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('No Hp'),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            controller: nohpController,
+                            decoration: InputDecoration(
+                              hintText: 'No HP',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(15),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(15),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 10, 86, 216),
+                                  width: 3.0,
+                                ),
+                              ),
+                              prefixIcon: Icon(Icons.call),
+                            ),
+                            autovalidateMode: AutovalidateMode.onUserInteraction,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'No HP tidak boleh kosong';
+                              }
+                              return null;
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Password'),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            controller: passwordController,
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(15),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(15),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 10, 86, 216),
+                                  width: 3.0,
+                                ),
+                              ),
+                              prefixIcon: Icon(Icons.lock),
+                              suffixIcon: IconButton(
+                                onPressed: liatpw,
+                                icon: Icon(
+                                  obscurepw ? Icons.visibility_off : Icons.visibility,
+                                ),
+                                color: Colors.grey,
+                              ),
+                            ),
+                            obscureText: obscurepw,
+                            autovalidateMode: AutovalidateMode.onUserInteraction,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Password tidak boleh kosong';
+                              }
+                              return null;
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Konfirmasi Password'),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            controller: confirmpasswordController,
+                            decoration: InputDecoration(
+                              hintText: 'Konfirmasi Password',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(15),
+                                ),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(15),
+                                ),
+                                borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 10, 86, 216),
+                                  width: 3.0,
+                                ),
+                              ),
+                              prefixIcon: Icon(Icons.lock),
+                              suffixIcon: IconButton(
+                                onPressed: liatpw,
+                                icon: Icon(
+                                  obscurepw ? Icons.visibility_off : Icons.visibility,
+                                ),
+                                color: Colors.grey,
+                              ),
+                            ),
+                            obscureText: obscurepw,
+                            autovalidateMode: AutovalidateMode.onUserInteraction,
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Konfrimasi Password tidak boleh kosong';
+                              }
+                              return null;
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
