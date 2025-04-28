0c85233c641a24410fe0d748b80666c1d41bc254
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-29 00:14:22 +0700

    Membuat Appbar data pelanggan

--- lib/pelanggan_page.dart
+++ lib/pelanggan_page.dart
@@ -29,7 +29,16 @@ class _PelangganPageState extends State<PelangganPage> {
 
   @override
   Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormState>();
     return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: EdgeInsets.only(left: 60.0),
+          child: Text('Data Pelanggan', style: TextStyle(color: Colors.white)),
+        ),
+        backgroundColor: Colors.blueAccent,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
     );
   }
 }
