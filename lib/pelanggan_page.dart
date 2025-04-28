d4661e04686615b959b645d4ded541f40a97804f
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-28 23:48:48 +0700

    Pelanggan Page input controller

---
+++ lib/pelanggan_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class PelangganPage extends StatefulWidget {
+  const PelangganPage({super.key});
+
+  @override
+  State<PelangganPage> createState() => _PelangganPageState();
+}
+
+class _PelangganPageState extends State<PelangganPage> {
+  final TextEditingController namaController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController nohpController = TextEditingController();
+  final TextEditingController alamatController = TextEditingController();
+  final TextEditingController provinsiController = TextEditingController();
+  final TextEditingController posController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
