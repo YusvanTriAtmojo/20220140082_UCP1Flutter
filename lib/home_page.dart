4ef402445833272c1a9499ff9648a0cdfc8728ec
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-28 22:41:55 +0700

    Home Page variabel email dan parameter mengisi datanya

---
+++ lib/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final String email;
+  const HomePage({super.key, required this.email});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
