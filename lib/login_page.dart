fa8eef29a986cf61751b0d44cb2d512006a24d5a
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-28 19:23:16 +0700

    Login Page controller input dan variabel obscurepw

---
+++ lib/login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool obscurepw = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
