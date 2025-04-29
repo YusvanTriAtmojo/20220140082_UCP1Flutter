e3e7e7ce17db4e5bc95241891a393ed6ac879c9e
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-29 20:26:56 +0700

    Body yang menampilkan data detail barang

--- lib/bukti_page.dart
+++ lib/bukti_page.dart
@@ -23,6 +23,75 @@ class BuktiPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image(image: AssetImage('assets/images/succes.png'),
+              height: 150,),
+              SizedBox(height: 60,),
+              Text('Data Berhasil Disimpan'),
+              SizedBox(height: 60,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Tanggal'),
+                  Text(tanggal),
+                ],
+              ),
+              Divider(height: 5,),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Jenis Transaksi'),
+                  Text(transaksi),
+                ],
+              ),
+              Divider(height: 5,),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Jenis Barang'),
+                  Text(barang),
+                ],
+              ),
+              Divider(height: 5,),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Jumlah Barang'),
+                  Text(jumlahBrg),
+                ],
+              ),
+              Divider(height: 5,),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Harga Satuan'),
+                  Text('Rp. $harga'),
+                ],
+              ),
+              Divider(height: 5,),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Total Harga'),
+                  Text('Rp. $totalHarga'),
+                ],
+              ),
+              Divider(height: 5,),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
