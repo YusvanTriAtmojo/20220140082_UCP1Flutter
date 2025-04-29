a3d75232fd18b2b7550ea5b496d99204e8d679fe
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-29 10:46:29 +0700

    Menambahkan Appbar detail pelanggan

--- lib/detail_pelanggan_page.dart
+++ lib/detail_pelanggan_page.dart
@@ -22,6 +22,18 @@ class DetailPelangganPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: EdgeInsets.only(left: 65.0),
+          child: Text(
+            'Detail $nama',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        backgroundColor: Colors.blueAccent,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+    );
   }
 }
