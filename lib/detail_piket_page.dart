ae4bda958d3c2bb84843b30b01d46be4701aa00a
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-29 15:45:30 +0700

    Membuat Appbar detail data tugas

--- lib/detail_piket_page.dart
+++ lib/detail_piket_page.dart
@@ -15,6 +15,14 @@ class DetailPiketPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Padding(
+          padding: EdgeInsets.only(left: 60.0),
+          child: Text('Detail $tugas', style: TextStyle(color: Colors.white)),
+        ),
+        backgroundColor: Colors.blueAccent,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
     );
   }
 }
