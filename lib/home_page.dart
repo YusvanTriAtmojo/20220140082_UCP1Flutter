c0864c732e81e5f16955ee9a933d8aba59bdbf94
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-29 21:19:59 +0700

    Membungkus column didalam singlechildscrollview

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,151 +11,154 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: 150,
-            decoration: BoxDecoration(color: Colors.blueAccent),
-            child: 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(width: 10),
-                CircleAvatar(
-                  radius: 40,
-                  backgroundImage: AssetImage('assets/images/uyun.jpg'),
-                ),
-                SizedBox(width: 15),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: 55.0),
-                      child: Text(
-                        'Selamat Datang',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                    Text(
-                      email,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(width: 150,),
-                IconButton(
-                  iconSize: 30,
-                  color: Colors.white,
-                  onPressed: (){
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginPage()),
-                  );
-                }, icon: Icon(Icons.logout))
-              ],
-            ),
-          ),
-          SizedBox(height: 20,),
-          Image(image: AssetImage('assets/images/promo.jpg'),
-          height: 155,),
-          SizedBox(height: 40,),
-          Padding(
-            padding: EdgeInsets.all(20.0),
-            child: Row(
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => PiketPage(email: email),
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(175.5, 175.5),
-                    backgroundColor: Colors.blueAccent,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
+      body: SingleChildScrollView(
+        child: 
+        Column(
+          children: [
+            Container(
+              height: 150,
+              decoration: BoxDecoration(color: Colors.blueAccent),
+              child: 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(width: 10),
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundImage: AssetImage('assets/images/uyun.jpg'),
                   ),
-                  child: Column(
+                  SizedBox(width: 15),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Image(
-                        image: AssetImage('assets/images/piket.png'),
-                        height: 50,
+                      Padding(
+                        padding: EdgeInsets.only(top: 55.0),
+                        child: Text(
+                          'Selamat Datang',
+                          style: TextStyle(color: Colors.white),
+                        ),
                       ),
-                      SizedBox(height: 20,),
-                      Text('Data Piket'),
+                      Text(
+                        email,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ],
                   ),
-                ),
-                SizedBox(width: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
+                  SizedBox(width: 150,),
+                  IconButton(
+                    iconSize: 30,
+                    color: Colors.white,
+                    onPressed: (){
+                    Navigator.pushReplacement(
                       context,
-                      MaterialPageRoute(builder: (context) => PelangganPage(
-                        email: email,
-                      )),
+                      MaterialPageRoute(builder: (context) => LoginPage()),
                     );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(175.5, 175.5),
-                    backgroundColor: Colors.blueAccent,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  child: Column(
-                    children: [
-                      Image(
-                        image: AssetImage('assets/images/pelanggan.png'),
-                        height: 50,
-                      ),
-                      SizedBox(height: 20,),
-                      Text('Data Pelanggan'),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 10),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => BarangPage(
-                   email: email,
-                )),
-              );
-            },
-            style: ElevatedButton.styleFrom(
-              minimumSize: Size(370, 175.5),
-              backgroundColor: Colors.blueAccent,
-              foregroundColor: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
+                  }, icon: Icon(Icons.logout))
+                ],
               ),
             ),
-            child: Column(
-              children: [
-                Image(
-                  image: AssetImage('assets/images/barang.png'),
-                  height: 50,
-                ),
-                SizedBox(height: 20,),
-                Text('Barang Masuk/Keluar'),
-              ],
+            SizedBox(height: 20,),
+            Image(image: AssetImage('assets/images/promo.jpg'),
+            height: 155,),
+            SizedBox(height: 40,),
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PiketPage(email: email),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(175.5, 175.5),
+                      backgroundColor: Colors.blueAccent,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Image(
+                          image: AssetImage('assets/images/piket.png'),
+                          height: 50,
+                        ),
+                        SizedBox(height: 20,),
+                        Text('Data Piket'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => PelangganPage(
+                          email: email,
+                        )),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(175.5, 175.5),
+                      backgroundColor: Colors.blueAccent,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: Column(
+                      children: [
+                        Image(
+                          image: AssetImage('assets/images/pelanggan.png'),
+                          height: 50,
+                        ),
+                        SizedBox(height: 20,),
+                        Text('Data Pelanggan'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => BarangPage(
+                    email: email,
+                  )),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(370, 175.5),
+                backgroundColor: Colors.blueAccent,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Image(
+                    image: AssetImage('assets/images/barang.png'),
+                    height: 50,
+                  ),
+                  SizedBox(height: 20,),
+                  Text('Barang Masuk/Keluar'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
