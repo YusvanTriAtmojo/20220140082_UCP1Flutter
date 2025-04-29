ca7197cf78434da25e78dd59a2ee3a4c55fe3ec0
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-29 13:56:44 +0700

    Membuat fungsi memilih tanggal

--- lib/piket_page.dart
+++ lib/piket_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class PiketPage extends StatefulWidget {
   final String email;
@@ -12,6 +13,26 @@ class _PiketPageState extends State<PiketPage> {
   final TextEditingController tugasController = TextEditingController();
   final TextEditingController dateController = TextEditingController();
   List<Map<String, dynamic>> daftarpiket = [];
+
+  void pilihDate() async {
+    DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2200),
+    );
+    if (picked != null) {
+      String formattedDate = DateFormat(
+        'EEEE, dd MMMM yyyy',
+        'id_ID',
+      ).format(picked);
+      setState(() {
+        dateController.text = formattedDate.toString();
+      });
+    } else {
+      print('Pilih Tanggal');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
