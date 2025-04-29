7a26c36ca01dda15d7e331128f17ac75e936645f
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-29 13:37:09 +0700

    Piket Page variabel dan parameter parsing email

---
+++ lib/piket_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PiketPage extends StatefulWidget {
+  final String email;
+  const PiketPage({super.key, required this.email});
+
+  @override
+  State<PiketPage> createState() => _PiketPageState();
+}
+
+class _PiketPageState extends State<PiketPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
