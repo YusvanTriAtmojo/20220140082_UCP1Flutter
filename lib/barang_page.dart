c5988ffc8ff42bbf48d6fbec155d391498fa7a6f
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-29 17:39:53 +0700

    Barang Page controller input, variabel harga dan total

---
+++ lib/barang_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class BarangPage extends StatefulWidget {
+  const BarangPage({super.key});
+
+  @override
+  State<BarangPage> createState() => _BarangPageState();
+}
+
+class _BarangPageState extends State<BarangPage> {
+  final TextEditingController tanggalController = TextEditingController();
+  final TextEditingController jumlahBrgController = TextEditingController();
+  final TextEditingController hargaController = TextEditingController();
+  String hargaSatuan = "";
+
+  int totalHarga = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
