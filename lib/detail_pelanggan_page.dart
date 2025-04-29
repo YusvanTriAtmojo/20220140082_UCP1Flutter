6a7b809f2aa5b6896607405cd26cfad78cfb23ea
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-29 11:00:07 +0700

    Menampilkan data detail pelanggan

--- lib/detail_pelanggan_page.dart
+++ lib/detail_pelanggan_page.dart
@@ -34,6 +34,136 @@ class DetailPelangganPage extends StatelessWidget {
         backgroundColor: Colors.blueAccent,
         iconTheme: IconThemeData(color: Colors.white),
       ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 60,
+                      backgroundImage: AssetImage('assets/images/uyun.jpg'),
+                    ),
+                    SizedBox(height: 20),
+                    Text(nama, style: TextStyle(fontSize: 25)),
+                    SizedBox(height: 5),
+                    Text(email),
+                    SizedBox(height: 10),
+                    Text(nohp, style: TextStyle(fontSize: 15)),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+              Text('Alamat'),
+              SizedBox(height: 10),
+              TextFormField(
+                initialValue: alamat,
+                readOnly: true,
+                decoration: InputDecoration(
+                  hintText: 'Alamat',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    borderSide: BorderSide(
+                      color: Color.fromARGB(255, 10, 86, 216),
+                      width: 3.0,
+                    ),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Alamat tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Provinsi'),
+                        SizedBox(height: 10),
+                        TextFormField(
+                          initialValue: provinsi,
+                          readOnly: true,
+                          decoration: InputDecoration(
+                            hintText: 'Provinsi',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(15),
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(15),
+                              ),
+                              borderSide: BorderSide(
+                                color: Color.fromARGB(255, 10, 86, 216),
+                                width: 3.0,
+                              ),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Provinsi tidak boleh kosong';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Kode Pos'),
+                        SizedBox(height: 10),
+                        TextFormField(
+                          initialValue: kodepos,
+                          readOnly: true,
+                          decoration: InputDecoration(
+                            hintText: 'Kode Pos',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(15),
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(15),
+                              ),
+                              borderSide: BorderSide(
+                                color: Color.fromARGB(255, 10, 86, 216),
+                                width: 3.0,
+                              ),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Kode Pos tidak boleh kosong';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
