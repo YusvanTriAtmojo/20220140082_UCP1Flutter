cdc081fedbf95f8fb7cab378d5ef1700e1aeb8d3
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-29 16:02:49 +0700

    Menampilkan detail data piket

--- lib/detail_piket_page.dart
+++ lib/detail_piket_page.dart
@@ -23,6 +23,48 @@ class DetailPiketPage extends StatelessWidget {
         backgroundColor: Colors.blueAccent,
         iconTheme: IconThemeData(color: Colors.white),
       ),
+      body: Padding(
+        padding: EdgeInsets.all(30.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  tanggal,
+                  style: TextStyle(
+                    color: Colors.blueAccent,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Text(email, style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: 350,
+              height: 50,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(left: 20.0),
+                child: Text(tugas, style: TextStyle(color: Colors.white)),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
