af27b5a2dd60a4a2c7ba6a68bcf7f97ee260e781
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-04-29 15:35:16 +0700

    DetailPiket Page variabel dan parameter parsing data piket

---
+++ lib/detail_piket_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DetailPiketPage extends StatelessWidget {
+  final String email;
+  final String tanggal;
+  final String tugas;
+
+  const DetailPiketPage({
+    super.key,
+    required this.email,
+    required this.tanggal,
+    required this.tugas,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
